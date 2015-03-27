22d4bd1cd5d53530f6650025a5b861fe7399a025
Alexander Templeton <azide0x37@users.noreply.github.com>
2015-03-27 00:47:53 -0500

    cleanup, added class skeleton

--- chainLink.py
+++ chainLink.py
@@ -1,11 +1,13 @@
 
 """
-mshpScraper.py 
-Missouri State Highway Patrol scraper for crash data!
+chainLink.py 
+Mines data from the offender search provided by the Missouri Department of Corrections website.
 [azide0x37] Alexander Templeton (nepenthe.me)
+
 Arguments:
 Returns: pandas Data.Frame object
-""
+"""
+
 import urllib2
 import pandas as pd
 from collections import OrderedDict
@@ -14,6 +16,16 @@ from datetime import datetime
 url = https://web.mo.gov/doc/offSearchWeb/searchOffender.do
 selectors = {firstName = 'firstName', lastName = 'lastName', offenderId = 'docId'}
 
+class chainLink:
+  def __init__(self):
+    pass
+  
+  def __str__(self):
+    pass
+  
+  def __call__(self):
+    return '0'
+  
 #class description
 #select search option: default = id number optional = firstName & lastName
 #download appropriate page
@@ -21,3 +33,5 @@ selectors = {firstName = 'firstName', lastName = 'lastName', offenderId = 'docId
 #return table identifier: <TABLE border="0" cellpadding="4" cellspacing="2" class="displayTable">
 #extract data to dataFrame
 #return data
+myLink = chainLink()
+print myLink()
