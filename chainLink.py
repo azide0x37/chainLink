dc4b13615f27efd3d79ef637e231974a8bf948b8
Alexander Templeton <azide0x37@users.noreply.github.com>
2015-03-27 16:08:50 -0500

    prototype scraping code; comments

--- chainLink.py
+++ chainLink.py
@@ -13,17 +13,52 @@ import pandas as pd
 from collections import OrderedDict
 from datetime import datetime
 
-url = https://web.mo.gov/doc/offSearchWeb/searchOffender.do
-selectors = {firstName = 'firstName', lastName = 'lastName', offenderId = 'docId'}
-
 class chainLink:
-  def __init__(self):
-    pass
-  
+  def __init__(self, url = https://web.mo.gov/doc/offSearchWeb/searchOffender.do, selectors = 'docId'):
+    self.url = url
+    self.selectors = selectors
+    
+    opener = urllib2.build_opener()
+    opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+    self.response = opener.open(self.url)
+    
+    #Set better formatting for testing console print 
+    pd.set_option('display.max_rows', 500) 
+    pd.set_option('display.max_columns', 500) 
+    pd.set_option('display.width', 200) 
+
   def __str__(self):
-    pass
+    try:
+      return self.url, "?", selectors
+    except:
+      print "Unable to display url"
   
   def __call__(self):
+    webpageSouped = BeautifulSoup(self.response.read())
+    
+    #Strip headings, send to list
+    headingTable = webpageSouped.find('table',,)
+    headings = headingTable.findAll('td')
+    
+    dataTable = webpageSouped.find('table', border="0", cellpadding="4", cellspacing="2")
+    rows = dataTable.findAll('tr')
+    
+    dataset = []
+    
+    for tr in rows:
+      cols = OrderedDict()
+      counter = 0
+      
+      for td in cols[1:]:
+        text = ''.join(td.find(text=True))
+        try:
+          headings[counter] = text
+          counter += 1
+        except:
+          counter = 0
+          continue
+      dataset.append(row_data)
+      
     return '0'
   
 #class description
